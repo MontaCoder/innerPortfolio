debe55971b2eddfa92991e9ecab90781fa399a22
Montassar Hajri <montassarhajri@outlook.com>
2024-05-31 01:35:44 +0100

    All from Local env

---
+++ src/components/showcase/About.tsx
@@ -0,0 +1,160 @@
+import React from 'react';
+import me from '../../assets/pictures/currentme.webp';
+import meNow from '../../assets/pictures/workingAtComputer.png';
+import { Link } from 'react-router-dom';
+import ResumeDownload from './ResumeDownload';
+
+export interface AboutProps {}
+
+const About: React.FC<AboutProps> = (props) => {
+    return (
+        // add on resize listener
+        <div className="site-page-content">
+            {/* <img src={me} style={styles.topImage} alt="" /> */}
+            <h1 style={{ marginLeft: -16 }}>Welcome</h1>
+            <h3>I'm Montassar Hajri</h3>
+            <br />
+            <div className="text-block">
+                <p>
+                    I'm a software engineer
+                </p>
+                <br />
+                <p>
+                    Thank you for taking the time to check out my portfolio. I
+                    really hope you enjoy exploring it as much as I enjoyed
+                    building it. If you have any questions or comments, feel
+                    free to contact me using{' '}
+                    <Link to="/contact">this form</Link> or shoot me an email at{' '}
+                    <a href="mailto:montassarhajri@outlook.com">
+                        montassarhajri@outlook.com
+                    </a>
+                </p>
+            </div>
+            <ResumeDownload />
+            <div className="text-block">
+                <h3>About Me</h3>
+                <br />
+                <p>
+                    From a young age, I have had a curiosity about how things
+                    worked. This naturally led me to become absolutely obsessed
+                    with Lego and I fell in love with building things.
+                </p>
+                <br />
+                <div className="captioned-image">
+                    <img src={me} style={styles.image} alt="" />
+                    <p>
+                        <sub>
+                            <b>Figure 1:</b> A real photo of me when im at Elementary School :)
+                        </sub>
+                    </p>
+                </div>
+
+                <p>
+                    I started programming more seriously in high school,
+                    initially learning how to scrape and interact with websites.
+                    I went on to do a ton of passion projects, many of them with
+                    one of my closest friends
+                    . We worked on many projects together, including chat bots,
+                    multiple game projects, apps, and more. One of these
+                    projects is viewable on my{' '}
+                    <Link to="/projects/software">Software Projects</Link> page.
+                </p>
+                <br />
+                <br />
+                <br />
+                <div style={{}}>
+                    <div
+                        style={{
+                            flex: 1,
+                            textAlign: 'justify',
+                            alignSelf: 'center',
+                            flexDirection: 'column',
+                        }}
+                    >
+                        <h3>My Hobbies</h3>
+                        <br />
+                        <p>
+                            Beyond software, I have a lot of hobbies that I
+                            enjoy doing in my free time. The more tangible
+                            hobbies I have are Music
+                            and creating
+                            Digital Art. Some other hobbies I
+                            enjoy are working out, cooking, and (unsurprisingly)
+                            playing video games.
+                        </p>
+                        <br />
+                        {/* <p>
+                            In college, I was an active member in the fraternity
+                            Sigma Alpha Epsilon and held multiple positions in
+                            the chapter. I met a lot of amazing people through
+                            my fraternity and thoroughly enjoyed the community.
+                        </p> */}
+                    </div>
+                    <div style={styles.verticalImage}>
+                        <img src={meNow} style={styles.image} alt="" />
+                        <p>
+                            <sub>
+                                <b>Figure 2:</b> Me, April 2022
+                            </sub>
+                        </p>
+                    </div>
+                </div>
+                <br />
+                <br />
+                <p>
+                    Thanks for reading about me! I hope that you enjoy exploring
+                    the rest of my portfolio website and everything it has to
+                    offer. If you find the easter egg make sure to let me know
+                    on Instagram{' '}
+                    <a
+                        rel="noreferrer"
+                        target="_blank"
+                        href="https://www.instagram.com/MontaCoder/"
+                    >
+                        @haadijmldn
+                    </a>{' '}
+                    Good luck and have fun!
+                </p>
+                <br />
+                <p>
+                    If you have any questions or comments I would love to hear
+                    them. You can reach me through the{' '}
+                    <Link to="/contact">contact page</Link> or shoot me an email
+                    at{' '}
+                    <a href="mailto:montassarhajri@outlook.com">
+                        montassarhajri@outlook.com
+                    </a>
+                </p>
+            </div>
+        </div>
+    );
+};
+
+const styles: StyleSheetCSS = {
+    contentHeader: {
+        marginBottom: 16,
+        fontSize: 48,
+    },
+    image: {
+        height: 'auto',
+        width: '100%',
+    },
+    topImage: {
+        height: 'auto',
+        width: '100%',
+        marginBottom: 32,
+    },
+    verticalImage: {
+        alignSelf: 'center',
+        // width: '80%',
+        marginLeft: 32,
+        flex: 0.8,
+
+        alignItems: 'center',
+        // marginBottom: 32,
+        textAlign: 'center',
+        flexDirection: 'column',
+    },
+};
+
+export default About;
